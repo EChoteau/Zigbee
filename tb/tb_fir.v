8b179a9ab69ccde741b6aab32f6808568db61a0c
Diaa MOHAMED <diaa.shehab10@gmail.com>
2026-03-06 11:11:08 +0100

    merged compile files and added tb for filter

---
+++ tb/tb_fir.v
@@ -0,0 +1,151 @@
+`timescale 1ns/1ps
+
+module tb_fir;
+
+    // =========================
+    // Parameters
+    // =========================
+
+    parameter CLK_PERIOD = 20;   // 50 MHz
+
+    // =========================
+    // Signals
+    // =========================
+
+    reg clk;
+    reg rst;
+    reg sample_en;
+    reg signed [5:0] x_in;
+    wire signed [22:0] y_out;
+
+    // =========================
+    // DUT
+    // =========================
+
+    fir_top DUT (
+        .clk(clk),
+        .rst(rst),
+        .sample_en(sample_en),
+        .x_in(x_in),
+        .y_out(y_out)
+    );
+
+    // =========================
+    // Clock generation
+    // =========================
+
+    initial begin
+        clk = 0;
+        forever #(CLK_PERIOD/2) clk = ~clk;
+    end
+
+    // =========================
+    // Stimulus
+    // =========================
+
+    integer i;
+
+    initial begin
+
+        // Init
+        rst = 0;
+        sample_en = 0;
+        x_in = 0;
+
+        #(5*CLK_PERIOD);
+
+        rst = 1;
+        sample_en = 1;
+
+        // =====================================
+        // Test impulse response
+        // =====================================
+
+        $display("---- Impulse Response Test ----");
+
+        x_in = 20;  // impulsion
+        #(CLK_PERIOD);
+
+        x_in = 0;
+
+        // Wait 40 cycles
+        for (i = 0; i < 40; i = i + 1)
+            #(CLK_PERIOD);
+
+        // =====================================
+        // Test Sinus - basse freq
+        // =====================================
+
+        $display("---- Low Frequency Sine Test ----");
+
+        for (i = 0; i < 200; i = i + 1) begin
+            x_in = 20 * $sin(2*3.14159*i/40);
+            #(CLK_PERIOD);
+        end
+
+        // =====================================
+        // Test Sinus - Haute Freq
+        // =====================================
+
+        $display("---- High Frequency Test ----");
+
+        for (i = 0; i < 200; i = i + 1) begin
+            x_in = 6'sd20 * $sin(2*3.14159*i/4);
+            #(CLK_PERIOD);
+        end
+
+        $display("Simulation finished.");
+        $stop;
+
+    end
+
+endmodule
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
