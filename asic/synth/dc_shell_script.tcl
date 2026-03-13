b44862df748f9b24b3c4fbab01df447a632bbedf
cordic team <cordic@mail>
2026-03-13 13:52:05 +0100

    ajout des scripts de compilation synthese

---
+++ asic/synth/dc_shell_script.tcl
@@ -0,0 +1,43 @@
+# --- 1. Setup ---
+#set_app_var target_library "work.db"
+#set_app_var link_library "* $target_library"
+
+remove_design -all
+#sh rm -rf WORK
+
+# --- 2. Lecture ---
+analyze -library WORK -format sverilog { \
+    ../../rtl/cordic/Boxcar_filter.sv \
+    ../../rtl/cordic/Cordic_init.sv \
+    ../../rtl/cordic/Cordic_step.sv \
+    ../../rtl/cordic/Cordic_system_complete.sv \
+    ../../rtl/cordic/Cordic_top.sv \
+    ../../rtl/cordic/Cordic_top_pipeline.sv \
+    ../../rtl/cordic/Derivate.sv \
+}
+
+elaborate cordic_system_complete -library WORK
+current_design cordic_system_complete
+link
+
+# --- 3. Contraintes ---
+create_clock -name clk -period 20 clk
+#set_clock_uncertainty 2 clk
+set_max_area 0
+
+# --- 4. Synthèse ---
+current_design cordic_system_complete
+ungroup -all -flatten
+compile_ultra -gate_clock
+# (Optionnel)
+# compile_ultra -incremental
+
+
+
+# --- 5. Rapports ---
+# On crée un dossier 'reports' pour ne pas polluer l'espace de travail
+file mkdir reports
+report_timing > reports/timing.rpt
+report_area   > reports/area.rpt
+report_power  > reports/power.rpt
+report_constraint -all_violators > reports/violations.rpt
