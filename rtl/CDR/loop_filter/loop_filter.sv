c96c5bfee22a71715583a4a88f9e174c78737071
regism <regismayaba@gmail.com>
2026-04-14 09:42:28 +0200

    initial commit of CDR

---
+++ rtl/CDR/loop_filter/loop_filter.sv
@@ -0,0 +1,43 @@
+module loop_filter #
+
+(
+    parameter WIDTH = 16
+)
+
+(
+    input wire clk,
+    input wire rst,
+
+    input wire up,
+    input wire down,
+
+    output reg signed [WIDTH-1:0] ctrl
+);
+logic up_l,down_l;
+always @(posedge clk or negedge rst) begin
+
+    if (~rst)
+    begin
+        ctrl <= 0;
+    end
+    else begin
+        //count<=count+1;
+        //if(count==5'h18)
+        //begin 
+        //    count<=0;
+            up_l <= up;
+            down_l <= down;
+            if (up & ~up_l)
+                ctrl <= ctrl + 10;
+
+            else if (down & down_l)
+                ctrl <= ctrl - 10;
+            else ctrl <= ctrl;
+       // end
+        
+
+    end
+
+end
+
+endmodule
