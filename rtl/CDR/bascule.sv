3667ca5c8b3b38dd0ec892bcaa7bc1bc81e6337c
regism <regismayaba@gmail.com>
2026-04-16 16:25:28 +0200

    commit intermediaire

--- rtl/CDR/bascule.sv
+++ rtl/CDR/bascule.sv
@@ -1,23 +1,23 @@
-module bascule(ck,en,rst,D,Q);
-    input wire ck,en,rst,D;
-    output reg Q;
-    reg en_d;
+module bascule(i_ck,i_en,i_rst,i_D,o_Q);
+    input wire i_ck,i_en,i_rst,i_D;
+    output reg o_Q;
+    reg s_en_d;
     `ifdef non_behaviour_model
     
-        bascule_temp(.ck(ck),.clear(rst),.D(D),.Q(Q));
+        bascule_temp(.ck(i_ck),.clear(i_rst),.D(i_D),.Q(i_Q));
      `else
-        always @(posedge ck or negedge rst)
+        always @(posedge i_ck or negedge i_rst)
             begin
-            if (~rst)
+            if (~i_rst)
                 begin
-                    Q <=1'b0;
+                    o_Q <=1'b0;
                 end
             else
                 begin
-                en_d <=en;
-                if(en & ~en_d)
+                s_en_d <= i_en;
+                if(i_en & ~s_en_d)
                 begin
-                    Q <=D;
+                    o_Q <=i_D;
                     end
                 end
             end
