0f2b496150b7622d724b9c9391fffdf7bd0d6eb2
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-04-16 15:55:37 +0200

    Made it so the top module is derivate for testing

---
+++ asic/synth/test_dc_shell_script.tcl
@@ -0,0 +1,46 @@
+# --- 1. Setup ---
+#set_app_var target_library "work.db"
+#set_app_var link_library "* $target_library"
+
+remove_design -all
+#sh rm -rf WORK
+
+# --- 2. Lecture ---
+analyze -library WORK -format sverilog {
+    ../../rtl/cordic/Derivate.sv
+}
+
+elaborate test_system_complete -library WORK
+current_design test_system_complete
+link
+
+# --- 3. Contraintes ---
+create_clock -name i_clk -period 100 {i_clk}
+set_clock_uncertainty 5 i_clk
+set_max_area 0
+
+# --- 4. Synthèse ---
+current_design test_system_complete
+ungroup -all -flatten
+compile_ultra -gate_clock
+# (Optionnel)
+# compile_ultra -incremental
+
+
+# --- 5. Rapports ---
+# On crée un dossier 'reports' pour ne pas polluer l'espace de travail
+file mkdir reports
+report_timing > reports/timing.rpt
+report_area   > reports/area.rpt
+report_power  > reports/power.rpt
+report_constraint -all_violators > reports/violations.rpt
+
+
+# --- 6. Export Files for Simulation ---
+# Write the Gate-Level Netlist
+write -format verilog -hierarchy -output netlist/test_system_complete.v
+
+# Write the SDF timing file
+write_sdf netlist/test_system_complete.sdf
+
+exit
