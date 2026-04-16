1b2ea567b92d0d6b09a377f6685c09bdefaff872
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-04-16 23:22:19 +0200

    test3

--- asic/pnr/top/scripts/design_config.tcl
+++ asic/pnr/top/scripts/design_config.tcl
@@ -133,6 +133,9 @@ globalNetConnect gnd3o! -type pgpin -pin gnd3o! -inst PWR* -module {}
 globalNetConnect vdd! -type pgpin -pin A -inst PWR* -module {}
 globalNetConnect gnd! -type pgpin -pin A -inst GND* -module {}
 
+globalNetConnect vdd3r1! -type pgpin -pin vdd3r1! -inst GND* -module {}
+globalNetConnect vdd3r2! -type pgpin -pin vdd3r2! -inst GND* -module {}
+globalNetConnect vdd3o! -type pgpin -pin vdd3o! -inst GND* -module {}
 globalNetConnect gnd! -type pgpin -pin gnd3r! -inst GND* -module {}
 globalNetConnect gnd! -type pgpin -pin gnd3o! -inst GND* -module {}
 
