be7d8ecfa28b5a0b290b485d6b7c4baa38ac955a
RIVIERE <eliot.riviere@grenoble-inp.org>
2026-04-17 14:07:23 +0200

    add: missing files and flow extended

--- asic/interface/pnr/top/scripts/flow.tcl
+++ asic/interface/pnr/top/scripts/flow.tcl
@@ -2,7 +2,27 @@
 # DEROULEMENT DU FLOW DE CONCEPTION
 #////////////////////////////////////////////////////
 
-# Init le design (netlist, lef, alims, etc..)
+# Design initialization (netlist, lef, alims, ...)
 source ../scripts/init.tcl
 init_design
 
+# Pad creation + grid creation + power connection
+source ../scripts/design_config.tcl
+setAnalysisMode -analysisType onChipVariation
+
+# Floorplan saving
+saveDesign dbs/floorplan_enc
+
+# Placement des standard cells
+source ../scripts/placement.tcl
+
+# Design saving afer placement
+saveDesign dbs/prects_enc
+
+# Clock tree synthesis
+source ../scripts/clock_tree_synthesis.tcl
+saveDesign dbs/postcts_enc
+
+# Open GUI for inspection
+win
+suspend
