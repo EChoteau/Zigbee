35a84a00371f951203ae585fe16814dd349f3bff
RIVIERE <eliot.riviere@grenoble-inp.org>
2026-04-17 18:37:25 +0200

    add: fillers

--- asic/interface/pnr/top/scripts/flow.tcl
+++ asic/interface/pnr/top/scripts/flow.tcl
@@ -2,26 +2,44 @@
 # DEROULEMENT DU FLOW DE CONCEPTION
 #////////////////////////////////////////////////////
 
-# Design initialization (netlist, lef, alims, ...)
+setMultiCpuUsage -localCpu 8
+
+# Init le design (netlist, lef, alims, etc..)
 source ../scripts/init.tcl
 init_design
 
-# Pad creation + grid creation + power connection
+# Placement des pads + creation de la grille + connexion des pads d'alimentation a la grille
 source ../scripts/design_config.tcl
 setAnalysisMode -analysisType onChipVariation
 
-# Floorplan saving
 saveDesign dbs/floorplan_enc
 
 # Placement des standard cells
 source ../scripts/placement.tcl
 
-# Design saving afer placement
 saveDesign dbs/prects_enc
 
-# Clock tree synthesis
+# Création du clock_tree
 source ../scripts/clock_tree_synthesis.tcl
 saveDesign dbs/postcts_enc
+
+# Ajout des fillers in core and pads
+source ../scripts/add_fillers.tcl
+saveDesign dbs/addFiller_enc
+
+routeDesign
+
+# Vérifications physiques finales après routage
+verifyGeometry
+verifyConnectivity
+
+# Vérification DRC finale après routage
+verify_drc
+
+# Exports post-route pour simulation back-annotée
+file mkdir ../output_data
+saveNetlist ../output_data/${init_top_cell}_postroute.v
+write_sdf ../output_data/${init_top_cell}_postroute.sdf
 
 # Open GUI for inspection
 win
