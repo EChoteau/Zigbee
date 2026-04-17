53fef4dbf897e3649efbe1957d77bcee79ed6121
regism <regismayaba@gmail.com>
2026-04-17 15:21:40 +0200

    update

--- rtl/CDR/decision.sv
+++ rtl/CDR/decision.sv
@@ -7,8 +7,8 @@ parameter resolution_in=6)
     output wire o_decision_out
 );
 
-assign o_decision_out = (i_dphi_in >  $signed(6'sd5)) ? 1'b1 :
-                        (i_dphi_in < -$signed(6'sd5)) ? 1'b0 :
+assign o_decision_out = (i_dphi_in > 6'sd5) ? 1'b1 :
+                        (i_dphi_in < -6'sd5) ? 1'b0 :
                         1'b0;  // zone morte
 
 endmodule
