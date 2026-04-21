f8f898a1fcf53a7c38450eb7a4ca1a9aaf387dc4
Diaa MOHAMED <diaa.shehab10@gmail.com>
2026-04-21 09:16:07 +0200

    script demod

--- asic/synth/demod/demod_dc_shell_script.tcl
+++ asic/synth/demod/demod_dc_shell_script.tcl
@@ -7,8 +7,6 @@ remove_design -all
 
 # --- 2. Lecture ---
 analyze -library WORK -format sverilog { \
-    ../../rtl/demod/FIR/coeff_rom.v \
-    ../../rtl/demod/FIR/delay_line.v \
     ../../rtl/demod/FIR/fir_core.v \
     ../../rtl/demod/FIR/fir_top.v \
     ../../rtl/demod/WAVE/demod.sv \
