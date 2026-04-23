a7b7cf3149cf6f750b022c8683545209df82af7b
BAHAMI <hosnibahami0@gmail.com>
2026-04-23 15:05:30 +0200

    bug fix msk

--- asic/synth/msk/msk_system_complete.tcl
+++ asic/synth/msk/msk_system_complete.tcl
@@ -2,11 +2,11 @@ remove_design -all
 
 # --- 2. Lecture ---
 analyze -library WORK -format sverilog { \
-    ./../rtl/msk/encodeur_diff.sv \
-    ./../rtl/msk/demux_msk.sv \
-    ./../rtl/msk/shaping_msk_cfg.sv \
-    ./../rtl/msk/top_msk_cfg.sv \
-    ./../tb/msk/top_msk_cfg.sv \
+    ./../../rtl/msk/encodeur_diff.sv \
+    ./../../rtl/msk/demux_msk.sv \
+    ./../../rtl/msk/shaping_msk_cfg.sv \
+    ./../../rtl/msk/top_msk_cfg.sv \
+    ./../../tb/msk/top_msk_cfg.sv \
 }
 
 elaborate top_msk -library WORK
