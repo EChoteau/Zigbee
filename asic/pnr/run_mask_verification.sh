0d626a829553c783c1308522543b4442a631e529
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-04-28 15:35:00 +0200

    test1

--- asic/pnr/run_mask_verification.sh
+++ asic/pnr/run_mask_verification.sh
@@ -16,5 +16,6 @@ source ../../../config/bashrc_cdsic617_ams_410_isr15
 
 # Launch Virtuoso with mask verification script
 export MODULE_NAME=$MODULE
-ams_cds -tech c35b4 -mode fb &
+#ams_cds -tech c35b4 -mode fb &
+ams_cds -tech c35b4 -mode fb -execute "source ../scripts/mask_verification.il" &
 #virtuoso -batch -nowin -execute "source ../scripts/mask_verification.il" &
