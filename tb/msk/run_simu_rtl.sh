38a367dada4810cb23257bef7abd49b3cfae126a
EL KASSIMI <elkassimizainab@gmail.com>
2026-04-28 13:57:46 +0200

    Update msk

--- tb/msk/run_simu_rtl.sh
+++ tb/msk/run_simu_rtl.sh
@@ -1,7 +1,8 @@
-vlib work 
+vdel -all -lib work
+vlib work
+vmap work work
 
-vmap work work 
+vlog -sv ../../rtl/msk/*.sv -work work
+vlog -sv top_msk_tb.sv -work work
 
-vlog ./../rtl/msk/encodeur_diff.sv ./../rtl/msk/demux_msk.sv ./../rtl/msk/shaping_msk_cfg.sv ./../rtl/msk/top_msk_cfg.sv ./../tb/msk/top_msk_cfg.sv 
-
-vsim -voptargs="+acc" work.top_msk_tb
+vsim -voptargs=+acc work.top_msk_tb
