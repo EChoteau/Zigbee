b713e9922c41cd1c539bd5685e1b368a547d7741
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-04-29 09:01:10 +0200

    Added comments to mask verify script

--- asic/pnr/run_mask_verification.sh
+++ asic/pnr/run_mask_verification.sh
@@ -17,5 +17,10 @@ source ../../../config/bashrc_cdsic617_ams_410_isr15
 # Launch Virtuoso with mask verification script
 export MODULE_NAME=$MODULE
 ams_cds -tech c35b4 -mode fb &
+#calibre->checkDRC
+#Run Control -> Multi thread
+#Ignore INFO_---
+#Ignore Ratio Metal
+#calibre -drc .calibreDRCRunset -hier -turbo -threads 8 -drc_runset .calibreDRCRunset cordic_system.gds cordic_system.drc
 #ams_cds -tech c35b4 -mode fb -execute "source ../scripts/mask_verification.il" &
 #virtuoso -batch -nowin -execute "source ../scripts/mask_verification.il" &
