6ae21d184d09355391e613bc9f3f2021eccdeb9d
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-04-30 15:23:55 +0200

    Demod wrapper pin fix

--- rtl/top/wrappers/demod_wrapper.sv
+++ rtl/top/wrappers/demod_wrapper.sv
@@ -11,8 +11,8 @@
 >>>>>>> 2822042 (Add CDR, Demod, and MSK wrappers to zigbee_chip_top; update interface_wrapper for new debug signals)
 module demod_wrapper #(
     parameter int CFG_WIDTH   = 3,
-    parameter int BUS_A_WIDTH = 10,
-    parameter int BUS_B_WIDTH = 12,
+    parameter int BUS_A_WIDTH = 12,
+    parameter int BUS_B_WIDTH = 10,
     parameter int BUS_C_WIDTH = 12,
     parameter int BUS_D_WIDTH = 2
 )(
