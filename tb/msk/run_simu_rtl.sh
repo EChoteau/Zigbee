6de8d1b344aabb3bfc06e34abf132a223986856a
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-04-30 15:26:09 +0200

    Renamed top_msk to msk_system

--- tb/msk/run_simu_rtl.sh
+++ tb/msk/run_simu_rtl.sh
@@ -3,6 +3,6 @@ vlib work
 vmap work work
 
 vlog -sv ../../rtl/msk/*.sv -work work
-vlog -sv top_msk_tb.sv -work work
+vlog -sv msk_system_tb.sv -work work
 
-vsim -voptargs=+acc work.top_msk_tb
+vsim -voptargs=+acc work.msk_system_tb
