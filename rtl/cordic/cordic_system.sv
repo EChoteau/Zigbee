b655aa6f6074eda6273008c3d1a79c73976034c8
leborgne <emilien.le-borgne@grenoble-inp.org>
2026-05-05 10:24:22 +0200

    Mapping wire top system (cordic+deriv+filtre)

--- rtl/cordic/cordic_system.sv
+++ rtl/cordic/cordic_system.sv
@@ -8,18 +8,40 @@ module cordic_system #(
     input  logic i_rst_n,
     input  logic signed [WIDTH_IN-1:0] i_i,
     input  logic signed [WIDTH_IN-1:0] i_q,
-    output logic signed [WIDTH_PHASE-1:0] o_phase
+    output logic signed [WIDTH_PHASE-1:0] o_phase,
+
+    // All intermediate signals for wrapper visibility
+    // w_signal_module_out is signal that comes out of module and is used as input to next module
+    // w_signal_module_in is signal that comes into the nextmodule and is driven by module
+    output  logic signed [WIDTH_PHASE-1:0] o_phase_cordic,         // Output of cordic
+    input   logic signed [WIDTH_PHASE-1:0] i_phase_to_derivative,   //input to derivate 
+    output  logic signed [WIDTH_PHASE-1:0] o_phase_derivative,      // Output of derivative,
+    input   logic signed [WIDTH_PHASE-1:0] i_phase_to_boxcar,       // input to boxcar filter
+
+    //flag to indicate if the system is being used in wrapper or standalone, used to control internal muxing of signals
+    input logic i_wrapper_flag
+
 );
 
-    logic signed [WIDTH_PHASE-1:0] w_phase_cordic;
-    logic signed [WIDTH_PHASE-1:0] w_phase_deriv;
 	logic signed [WIDTH_IN-1:0] s_i_buf;
 	logic signed [WIDTH_IN-1:0] s_q_buf;
+    logic signed [WIDTH_PHASE-1:0] w_phase_cordic;
+    logic signed [WIDTH_PHASE-1:0] w_phase_to_derivative;
+    logic signed [WIDTH_PHASE-1:0] w_phase_derivative;
+    logic signed [WIDTH_PHASE-1:0] w_phase_to_boxcar;
+
+    assign o_phase_cordic       = w_phase_cordic;
+    assign o_phase_derivative   = w_phase_derivative;
+    
+    // Si wrapper_flag == 1 : utilise inputs externes, sinon : utilise signaux internes
+    assign w_phase_to_derivative = i_wrapper_flag ? i_phase_to_derivative : w_phase_cordic;
+    assign w_phase_to_boxcar     = i_wrapper_flag ? i_phase_to_boxcar : w_phase_derivative;
+    
 
     always_ff @(posedge i_clk or negedge i_rst_n) begin
 	if (!i_rst_n) begin
 	    s_q_buf <= '0;
-        s_i_buf <= 'd1;
+        s_i_buf <= 'd1; // Avoid undefined phase at reset (arctan(0/0) is undefined, arctan(1/0) is 90 degrees)
 	end else begin
 	    s_q_buf <= i_q;
 	    s_i_buf <= i_i;
@@ -33,7 +55,7 @@ module cordic_system #(
         .i_clk(i_clk),
         .i_rst_n(i_rst_n),
         .i_i(s_i_buf), .i_q(s_q_buf),
-        .o_phase(w_phase_cordic)
+        .o_phase(o_phase_cordic)
     );
 
     derivative #(
@@ -41,8 +63,8 @@ module cordic_system #(
     ) derivative_inst (
         .i_clk(i_clk),
         .i_rst_n(i_rst_n),
-        .i_phase(w_phase_cordic),
-        .o_phase_deriv(w_phase_deriv)
+        .i_phase(w_phase_to_derivative),
+        .o_phase_deriv(o_phase_derivative)
     );
 
     boxcar_filter #(
@@ -50,7 +72,7 @@ module cordic_system #(
         .N(FILTER_N)
     ) boxcar_filter_inst (
         .i_clk(i_clk), .i_rst_n(i_rst_n),
-        .i_data(w_phase_deriv), .o_data(o_phase)
+        .i_data(w_phase_to_boxcar), .o_data(o_phase)
     );
 
 endmodule
--- tb/cordic/complete_tb.sv
+++ tb/cordic/complete_tb.sv
@@ -17,6 +17,7 @@ module complete_tb();
 	    .i_clk(i_clk), .i_rst_n(i_rst_n),
         .i_i(i_i), .i_q(i_q),
         .o_phase(o_phase)
+        .i_wrapper_flag(1'b0) // Set wrapper flag to 0 for direct testing of cordic system
     );
 
     // --- Clock Generation ---
