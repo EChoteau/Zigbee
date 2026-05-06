9d78d7f11effa6710a8a47c401f7319c05444003
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-05-06 11:43:57 +0200

    Squashed tb-top commits into new branch tb-zigbeetop

---
+++ tb/top/configs/4_demod/config_header.svh
@@ -0,0 +1,11 @@
+task automatic test_4_demod();
+    begin
+        $display("[%0t] test_4_demod", $time);
+        i_top_cfg = 3'd4;
+        i_wrapper_cfg = 3'b000;
+        rst_n = 0;
+        repeat (2) @(posedge clk);
+        rst_n = 1;
+        repeat (20) @(posedge clk);
+    end
+endtask
