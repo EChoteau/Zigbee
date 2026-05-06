c110483fc7c7168cfbde87d07b63fc514b7b81ce
RIVIERE <eliot.riviere@grenoble-inp.org>
2026-05-06 14:11:55 +0200

    add synth script for top with gitgnore uptaded

---
+++ asic/synth/top/script.sh
@@ -0,0 +1,80 @@
+## SCRIPT SYNTHESIS
+#
+# To run this script
+# Be in Zigbee root directory and run :
+# $ source asic/synth/top/script.sh
+#
+# This script will run the synthesis of the top level design (zigbee_top.sv) using design_vision.
+
+echo ""
+echo "========================================"
+echo "           RUNNING SYNTHESIS            "
+echo "========================================"
+echo ""
+echo "      FOR TOP LEVEL : zigbee_top.sv     "
+echo ""
+echo "----------------------------------------"
+
+
+source config/config_ASIC
+cd asic/synth/top
+
+#Clean previous work
+rm -rf work
+mkdir work
+cd work
+
+#Run design_vision for synthesis
+dc_shell -f ../synth.tcl | tee log_synthese.log
+
+echo ""
+echo ""
+echo "========================================"
+echo "            SYNTHESIS SUMMARY           "
+echo "========================================"
+
+# Count errors and warnings in the log file
+NB_ERRORS=$(grep -i -c "error" log_synthese.log)
+NB_WARNINGS=$(grep -i -c "warning" log_synthese.log)
+
+if [ "$NB_ERRORS" -gt 0 ]; then
+    # \e[41m = red background, \e[97m = white text, \e[0m = reset
+    echo -e "\e[41m\e[97m FAIL \e[0m"
+    echo ""
+    
+    # Extract blocks from the command to the end of the log
+    # Design vision is on stop on fail so the log ends shortly after the error
+    BLOCK_COMPILE=$(sed -n '/compile_ultra/,$p' log_synthese.log)
+    BLOCK_ANALYZE=$(sed -n '/analyze -library/,$p' log_synthese.log)
+    BLOCK_ELABORATE=$(sed -n '/elaborate top/,$p' log_synthese.log)
+
+    # Check in reverse chronological order
+    if [ -n "$BLOCK_COMPILE" ] && echo "$BLOCK_COMPILE" | grep -i -q "error"; then
+        # \e[100m = gray background, \e[0m = reset
+        echo "=> Error located in phase : \e[100m compile_ultra \e[0m"
+        # Display the details :
+        echo "$BLOCK_COMPILE" | grep -i "error" | head -n 3
+
+    elif [ -n "$BLOCK_ELABORATE" ] && echo "$BLOCK_ELABORATE" | grep -i -q "error"; then
+        # \e[100m = gray background, \e[0m = reset
+        echo "=> Error located in phase : \e[100m elaborate \e[0m"
+        # Display the details :
+        echo "$BLOCK_ELABORATE" | grep -i "error" | head -n 3
+        
+    elif [ -n "$BLOCK_ANALYZE" ] && echo "$BLOCK_ANALYZE" | grep -i -q "error"; then
+        # \e[100m = gray background, \e[0m = reset
+        echo "=> Error located in phase : \e[100m analyze \e[0m"
+        # Display the details :
+        echo "$BLOCK_ANALYZE" | grep -i "error" | head -n 3
+    else
+        echo "=> Error phase not dynamically identified. See the complete log."
+    fi
+else
+    # \e[42m = green background, \e[97m = white text, \e[0m = reset
+    echo -e "\e[42m\e[97m DONE \e[0m"
+
+echo "----------------------------------------"
+echo -e " Total Errors   : $NB_ERRORS"
+echo -e " Total Warnings : $NB_WARNINGS"
+echo "========================================"
+
---
+++ asic/synth/top/synth.tcl
@@ -0,0 +1,97 @@
+# --- 1. Setup Environment ---
+#Stop on fail
+set sh_continue_on_error false
+
+#Result c
+file rm -rf reports
+file rm -rf netlist
+file mkdir reports
+file mkdir netlist
+
+#Clean previous design
+remove_design -all
+
+# --- 2. Read ---
+analyze -library WORK -format sverilog { \
+	../../../rtl/top/zigbee_top.sv \
+
+	../../../rtl/top/wrappers/msk_wrapper.sv \
+	../../../rtl/top/wrappers/demod_wrapper.sv \
+	../../../rtl/top/wrappers/cdr_wrapper.sv \
+	../../../rtl/top/wrappers/cordic_wrapper.sv \
+	../../../rtl/top/wrappers/interface_wrapper.sv \
+
+	../../../rtl/msk/demux_msk.sv \
+	../../../rtl/msk/encodeur_diff.sv \
+	../../../rtl/msk/shaping_msk.sv \
+	../../../rtl/msk/top_msk.sv \
+
+	../../../rtl/demod/FIR/fir_core.vs \
+	../../../rtl/demod/FIR/fir_top.vs \
+	../../../rtl/demod/WAVE/demod.sv \
+	../../../rtl/demod/WAVE/wave_generator.sv \
+	../../../rtl/demod/demod_system_top.sv \
+
+	../../../rtl/CDR/cdr.sv \
+	../../../rtl/CDR/hogge_phase_detector.sv \
+	../../../rtl/CDR/loop_filter.sv \
+	../../../rtl/CDR/nco.sv \
+	../../../rtl/CDR/decision.sv \
+	../../../rtl/CDR/bascule.sv \
+
+	../../../rtl/cordic/cordic_system.sv \
+	../../../rtl/cordic/cordic_top.sv \
+	../../../rtl/cordic/cordic_init.sv \
+	../../../rtl/cordic/cordic_step.sv \
+	../../../rtl/cordic/boxcar_filter.sv \
+	../../../rtl/cordic/cordic_top_pipeline.sv \
+	../../../rtl/cordic/cordic_top_hybride.sv \
+	../../../rtl/cordic/derivate.sv \
+
+	../../../rtl/interface/interface_top.sv \
+	../../../rtl/interface/serializer.sv \
+	../../../rtl/interface/fifo.sv \
+	../../../rtl/interface/deserializer.sv \
+	../../../rtl/interface/baud_rate_gen.sv \
+	../../../rtl/interface/apb_slave_regs.sv \
+	
+}
+
+current_design top
+elaborate top -library WORK
+link
+
+# --- 3. Constrains ---
+set_operating_conditions -library c35_CORELIB_TYP TYPICAL
+create_clock -name i_clk -period 100 {i_clk}
+#set_clock_uncertainty 15 i_clk
+set_max_area 0
+
+# --- 4. Synthesis ---
+current_design top
+set_ungroup top
+set_boundary_optimization top
+set_scan_configuration -style none
+set_flatten true -design top -effort high -minimize multiple_output -phase true
+set_structure true -design top -boolean true -timing false
+
+# set_max_fanout 3 top
+# set_max_transition 1.5 top
+set_dynamic_optimization true
+set_leakage_optimization true
+# set_max_dynamic_power 0
+# set_max_leakage_power 0
+compile_ultra -gate_clock
+
+# --- 5. Reports ---
+report_timing > reports/timing.rpt
+report_area > reports/area.rpt
+report_power > reports/power.rpt
+report_constraint -all_violators > reports/violations.rpt
+report_clock_gating > reports/report_cg_summary.txt
+
+# --- 6. Export Files for Simulation ---
+write -format verilog -hierarchy -output netlist/top_synth.v
+write_sdf netlist/top_synth.sdf
+
+exit
