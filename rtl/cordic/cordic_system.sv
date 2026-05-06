e3a71347a82f5b86369f7477558230109704e947
leborgne <emilien.le-borgne@grenoble-inp.org>
2026-05-06 08:47:04 +0200

    trl top wrong wiring

--- rtl/cordic/cordic_system.sv
+++ rtl/cordic/cordic_system.sv
@@ -13,10 +13,10 @@ module cordic_system #(
     // All intermediate signals for wrapper visibility
     // w_signal_module_out is signal that comes out of module and is used as input to next module
     // w_signal_module_in is signal that comes into the nextmodule and is driven by module
-    output  logic signed [WIDTH_PHASE-1:0] o_phase_cordic,         // Output of cordic
+    output  logic signed [WIDTH_PHASE-1:0] o_phase_cordic,          // Output of cordic
     input   logic signed [WIDTH_PHASE-1:0] i_phase_to_derivative,   //input to derivate 
     output  logic signed [WIDTH_PHASE-1:0] o_phase_derivative,      // Output of derivative,
-    input   logic signed [WIDTH_PHASE-1:0] i_phase_to_boxcar       // input to boxcar filter
+    input   logic signed [WIDTH_PHASE-1:0] i_phase_to_boxcar        // input to boxcar filter
 );
 
 	logic signed [WIDTH_IN-1:0] s_i_buf;
@@ -51,7 +51,7 @@ module cordic_system #(
         .i_clk(i_clk),
         .i_rst_n(i_rst_n),
         .i_i(s_i_buf), .i_q(s_q_buf),
-        .o_phase(o_phase_cordic)
+        .o_phase(w_phase_cordic)
     );
 
     derivative #(
@@ -60,7 +60,7 @@ module cordic_system #(
         .i_clk(i_clk),
         .i_rst_n(i_rst_n),
         .i_phase(w_phase_to_derivative),
-        .o_phase_deriv(o_phase_derivative)
+        .o_phase_deriv(w_phase_derivative)
     );
 
     boxcar_filter #(
