b7f3f6701cd45882cdf096c330ea5530087a8ea2
leborgne <emilien.le-borgne@grenoble-inp.org>
2026-05-06 11:38:39 +0200

    rename and move cordic_system_wrapper_tb (manque asserts)

--- tb/top/generic/apply_reset.svh
+++
@@ -1,9 +0,0 @@
-// Helper task: apply_reset
-task automatic apply_reset(int cycles);
-begin
-    i_rst_n = 1'b0;
-    repeat (cycles) @(posedge i_clk);
-    i_rst_n = 1'b1;
-    @(posedge i_clk);
-end
-endtask
--- tb/top/run_tb_top.sh
+++
@@ -1,7 +0,0 @@
-# =====================================================
-# Script qui lance les sims
-# =====================================================
-
-source config/config_RTL
-
-vsim -do tb/top/top_tb.do
