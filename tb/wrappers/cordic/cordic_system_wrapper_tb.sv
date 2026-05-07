d98d6f5e86017ebd0c88de2dfe81201ce974c16f
leborgne <emilien.le-borgne@grenoble-inp.org>
2026-05-07 09:12:32 +0200

    tb_wrapper fix

--- tb/wrappers/cordic/cordic_system_wrapper_tb.sv
+++ tb/wrappers/cordic/cordic_system_wrapper_tb.sv
@@ -87,7 +87,7 @@ module cordic_system_wrapper_tb();
         assert (!$isunknown(o_bus_c))
             else $error("%s: o_bus_c is not connected (contains X/Z)", label);
 
-        localparam logic is_close_enough = 1'b0;
+        logic is_close_enough = 1'b0;
 
         if ( observed > expected )
             is_close_enough = (observed - expected) <= error_margin; // observed can be slightly above expected
