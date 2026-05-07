29c7c39e9b8874e21780deb4758e39a2a980d431
leborgne <emilien.le-borgne@grenoble-inp.org>
2026-05-07 09:30:29 +0200

    tb_wrapper fix

--- tb/wrappers/cordic/cordic_system_wrapper_tb.sv
+++ tb/wrappers/cordic/cordic_system_wrapper_tb.sv
@@ -405,6 +405,13 @@ module cordic_system_wrapper_tb();
         endcase
     endtask
 
+    task automatic rst_wait();
+        i_rst_n = 1'b0;
+        repeat (4) @(posedge i_clk);
+        i_rst_n = 1'b1;
+        repeat (2) @(posedge i_clk);
+    endtask
+
 /*---------------- Test sequence control----------------*/
 
     initial begin
@@ -412,13 +419,21 @@ module cordic_system_wrapper_tb();
         $display("--- top_cordic_tb: starting wrapper_cfg sweep ---");
 
         run_cfg(MODE_0);    // default path: cordic -> filter
+        rst_wait();
         run_cfg(MODE_1);    // cordic -> cordic
+        rst_wait();
         run_cfg(MODE_2);    // derivate -> derivate
+        rst_wait();
         run_cfg(MODE_3);    // filter -> filter
+        rst_wait();
         run_cfg(MODE_4);    // cordic -> derivate
+        rst_wait();
         run_cfg(MODE_5);    // cordic -> filter (same as default, retest to check for consistency)
+        rst_wait();
         run_cfg(MODE_6);    // derivate -> filter
+        rst_wait();
         run_cfg(MODE_7);    // filter -> filter (same as MODE_3, retest to check for consistency)
+        rst_wait();
 
         $display("--- top_cordic_tb: finished ---");
         $finish;
