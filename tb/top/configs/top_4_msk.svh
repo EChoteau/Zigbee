5a22b48a710647f4a39f3ff6417fe9cb015d8864
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-05-12 09:40:59 +0200

    WIP: Include bugfixes

--- tb/top/configs/top_4_msk.svh
+++ tb/top/configs/top_4_msk.svh
@@ -1,12 +1,13 @@
+// MSK Wrapper tests - include task definitions at module level
+`include "tb/wrappers/msk/headers/msk_wrapper_normal.svh"
+`include "tb/wrappers/msk/headers/msk_wrapper_debug_enc.svh"
+`include "tb/wrappers/msk/headers/msk_wrapper_debug_demux.svh"
+`include "tb/wrappers/msk/headers/msk_wrapper_debug_shaping.svh"
+`include "tb/wrappers/msk/headers/msk_wrapper_debug_all.svh"
+`include "tb/wrappers/msk/headers/msk_wrapper_test_plan.svh"
+
 task automatic test_4_msk();
     begin
-        `include "tb/wrappers/msk/headers/msk_wrapper_normal.svh"
-        `include "tb/wrappers/msk/headers/msk_wrapper_debug_enc.svh"
-        `include "tb/wrappers/msk/headers/msk_wrapper_debug_demux.svh"
-        `include "tb/wrappers/msk/headers/msk_wrapper_debug_shaping.svh"
-        `include "tb/wrappers/msk/headers/msk_wrapper_debug_all.svh"
-        `include "tb/wrappers/msk/headers/msk_wrapper_test_plan.svh"
-
         $display("[%0t] test_4_msk", $time);
         i_top_cfg = 3'd4;
         i_wrapper_cfg = 3'b000;
