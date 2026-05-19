ee439a15b029ab96089e096b790098346f54ce2c
RIVIERE <eliot.riviere@grenoble-inp.org>
2026-05-19 11:56:29 +0200

    fix innovus output for virtuoso

--- asic/pnr/scripts/final_steps.tcl
+++ asic/pnr/scripts/final_steps.tcl
@@ -1,5 +1,5 @@
 #////////////////////////////////////////////////////
-# VERIFICATION ET EXPORT FINAL
+# FINAL VERIFICATION AND EXPORT (COMPLETE VERSION)
 #////////////////////////////////////////////////////
 
 set output_root "../output_data/${module_name}"
@@ -15,9 +15,7 @@ file mkdir $reports_dir
 file mkdir $fab_dir
 
 verifyGeometry
-
 verifyConnectivity
-
 verify_drc
 
 puts "=== Extract RC parasitics ==="
@@ -32,11 +30,22 @@ report_qor -file "$reports_dir/qor.rpt"
 
 puts "=== Save Design Files ==="
 saveDesign dbs/final_enc
+
+# Simulation netlist (without power pins)
 saveNetlist "$output_root/${module_name}_postroute.v"
+
+# VIRTUOSO / LVS: Physical netlist with VDD/VSS connected throughout the design
+puts "=== Save Physical Netlist for Virtuoso/LVS ==="
+saveNetlist "$output_root/${module_name}_lvs.v" -phys
+
 write_sdf "$output_root/${module_name}_postroute.sdf"
 
-puts "=== Export GDSII (mask layout) ==="
-streamOut "$fab_dir/${module_name}.gds" -mapFile $gds_map_file
+puts "=== Export GDSII ==="
+
+streamOut "$fab_dir/${module_name}_.gds" \
+    -mapFile $gds_map_file \
+    -mode ALL \
+    -outputMacros
 
 puts "=== Save DEF ==="
 defOut -floorplan -netlist -routing "$fab_dir/${module_name}.def"
