cb911a3e05389475e8bfba230eb9b9934654dcbd
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-05-19 09:36:44 +0200

    Normalized interface script

--- tb/interface/script/script1.sh
+++ tb/interface/script/script1.sh
@@ -1,9 +1,12 @@
+#!/bin/sh
+set -e
+
 vdel -all -lib lib_rtl
 vlib lib_rtl
 vmap lib_rtl lib_rtl
 
-vlog -sv ../../../rtl/interface/*.sv -work lib_rtl
-vlog -sv ../common/*.sv -work lib_rtl
-vlog -sv ../*.sv -work lib_rtl
+vlog -sv rtl/interface/*.sv -work lib_rtl
+vlog -sv tb/interface/common/*.sv -work lib_rtl
+vlog -sv tb/interface/*.sv -work lib_rtl
 
 vsim -voptargs=+acc lib_rtl.interface_top_tb
