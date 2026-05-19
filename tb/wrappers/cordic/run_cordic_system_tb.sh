6bf5e43a9baec05565568439185625c102f1620b
Ludovic Bouchard <ludovicb1239@gmail.com>
2026-05-19 10:29:17 +0200

    Made paths absolute

--- tb/wrappers/cordic/run_cordic_system_tb.sh
+++ tb/wrappers/cordic/run_cordic_system_tb.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 set -euo pipefail
-cd "$(dirname "$0")"
 
 # work library
 vdel -all -lib lib_rtl 2>/dev/null || true
 vlib lib_rtl
 vmap lib_rtl lib_rtl
 
-# compile RTL and TB
-vlog -sv ../../../rtl/cordic/*.sv -work lib_rtl
-vlog -sv ../../../rtl/top/wrappers/cordic_system_wrapper.sv -work lib_rtl
-vlog -sv ./tb_cordic_system_wrapper.sv -work lib_rtl
+# compile RTL and TB (must be run from project root)
+vlog -sv rtl/cordic/*.sv -work lib_rtl
+vlog -sv rtl/top/wrappers/cordic_system_wrapper.sv -work lib_rtl
+vlog -sv tb/wrappers/cordic/tb_cordic_system_wrapper.sv -work lib_rtl
 
 
-vlog -incr -sv -work lib_RTL +acc ../../../rtl/cordic/*.sv
-vlog -incr -sv -work lib_RTL +acc ../../../tb/cordic/*.sv
+vlog -incr -sv -work lib_RTL +acc rtl/cordic/*.sv
+vlog -incr -sv -work lib_RTL +acc tb/cordic/*.sv
 vlog -incr -sv -work lib_RTL +acc tb/wrappers/cordic/cordic_system_wrapper_tb.sv
 vlog -incr -sv -work lib_RTL +acc rtl/top/wrappers/cordic_system_wrapper.sv
 
