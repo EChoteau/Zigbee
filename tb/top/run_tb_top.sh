1631419be34bdfbd2180df7a13686cff1c9d2f59
choteaue <choteau@grenoble-inp.fr>
2026-05-29 10:31:24 +0200

    clean script coverage done

--- tb/top/run_tb_top.sh
+++ tb/top/run_tb_top.sh
@@ -32,7 +32,6 @@ vlog -incr -sv -work lib_RTL +acc tb/wrappers/cdr/cdr_wrapper_tasks_pkg.sv
 vlog -incr -sv -work lib_RTL +acc tb/top/top_tb.sv
 
 n=lib_RTL.top_tb
-#vsim -c ${n} -coverage -voptargs="+cover=${coverage_code}" -do "do tb/top/top_tb.do; coverage save -onexit ${coverage_ucdb}; coverage save -onexit -instance top_tb.uut ${coverage_core}; quit -f"
+
 vsim -c -coverage -voptargs="+cover=bcesft" lib_RTL.top_tb -do "coverage save -onexit global_cov.ucdb; run -all; quit"
 vcover report -file rapport_global.txt -details -zeros global_cov.ucdb
-#vcover report ${coverage_code} -details -testdetails > coverage_report.txt
