a811930c2c0cb8bdc99ad70162fcb7fa3b9a14dd
choteaue <choteau@grenoble-inp.fr>
2026-05-30 14:32:50 +0200

    update streamOut command in final_steps.tcl to use relative path for gds_layer.map

--- asic/pnr/scripts/final_steps.tcl
+++ asic/pnr/scripts/final_steps.tcl
@@ -43,7 +43,7 @@ write_sdf "$output_root/${module_name}_postroute.sdf"
 puts "=== Export GDSII ==="
 
 streamOut zigbee_top_io.gds \
-    -mapFile ../scripts/gds_layer.map \
+    -mapFile gds_layer.map \
     -merge [list /softslin/AMS_410_ISR15/cds/HK_C35/GDS/c35b4_cells.gds] \
     -attachNetName 1 \
     -units 1000 \
