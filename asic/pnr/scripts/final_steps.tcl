1edd97f38bef6492a106924f1fec38586ea2456d
choteaue <choteau@grenoble-inp.fr>
2026-05-30 15:28:40 +0200

    update streamOut command in final_steps.tcl to include parameters for GDSII export

--- asic/pnr/scripts/final_steps.tcl
+++ asic/pnr/scripts/final_steps.tcl
@@ -42,7 +42,12 @@ write_sdf "$output_root/${module_name}_postroute.sdf"
 
 puts "=== Export GDSII ==="
 
-streamOut zigbee_top_io.gds
+streamOut zigbee_top_io.gds \
+    -mapFile  /softslin/AMS_410_ISR15/cds/HK_C35/TECH_C35B4/TECH_C35B4.layermap\
+    -merge [list /softslin/AMS_410_ISR15/cds/HK_C35/GDS/c35b4_cells.gds] \
+    -attachNetName 1 \
+    -units 1000 \
+    -mode ALL
 
 puts "=== Save DEF ==="
 defOut -floorplan -netlist -routing "$fab_dir/${module_name}.def"
