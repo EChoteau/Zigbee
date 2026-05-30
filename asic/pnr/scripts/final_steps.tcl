56472df474b4cf1735b47e6b5afe510a9ba00004
choteaue <choteau@grenoble-inp.fr>
2026-05-30 18:46:38 +0200

    update design configuration and final steps for improved GDSII export and net connections

--- asic/pnr/scripts/final_steps.tcl
+++ asic/pnr/scripts/final_steps.tcl
@@ -43,10 +43,11 @@ write_sdf "$output_root/${module_name}_postroute.sdf"
 puts "=== Export GDSII ==="
 
 streamOut zigbee_top_io.gds \
-    -mapFile  gds2.map\
-    -attachNetName 1 \
-    -units 1000 \
-    -mode ALL
+    -mapFile gds2.map \
+    -libName DesignLib \
+    -attachInstanceName 13 \
+    -attachNetName 13 \
+    -stripes 1 -units 1000 -mode ALL
 
 puts "=== Save DEF ==="
 defOut -floorplan -netlist -routing "$fab_dir/${module_name}.def"
