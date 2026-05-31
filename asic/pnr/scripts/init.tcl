4917f7d9c3bff41c8031e2d2d58d55d6d7b1bf11
choteaue <choteau@grenoble-inp.fr>
2026-05-31 19:29:19 +0200

    Fix LEF file path for zigbee_top module to include IOLIB_4M.lef

--- asic/pnr/scripts/init.tcl
+++ asic/pnr/scripts/init.tcl
@@ -14,9 +14,9 @@ set init_mmmc_file "../scripts/view_definition.tcl"
 #set init_lef_file "/softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/c35b4.lef_back /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/CORELIB.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/IOLIB_4M.lef"
 set init_lef_file "/softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/c35b4.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/CORELIB.lef"
 if {$module_name eq "zigbee_top"} {
-	set init_lef_file "/softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/c35b4.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/CORELIB.lef ../input_data/IOLIB_4M.lef"
+	set init_lef_file "/softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/c35b4.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/CORELIB.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/IOLIB_4M.lef"
 }
-
+#set init_lef_file "/softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/c35b4.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/CORELIB.lef ../input_data/IOLIB_4M.lef"
 
 if {$module_name eq "zigbee_top"} {
 	set init_top_cell "${module_name}_io"
