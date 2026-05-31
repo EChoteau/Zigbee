221467391cefe5c4698af4e252c89d8f4bc1d184
choteaue <choteau@grenoble-inp.fr>
2026-05-31 18:52:34 +0200

    Refactor design configuration and flow scripts
    - Removed the wide enclosure rule creation from design_config.tcl and added it to flow.tcl for better organization and flow control.
    - Updated init.tcl to change the path for IOLIB_4M.lef based on the module name, ensuring correct LEF file usage for the zigbee_top module.

--- asic/pnr/scripts/flow.tcl
+++ asic/pnr/scripts/flow.tcl
@@ -45,6 +45,15 @@ puts "\n======================================================\n--- 5. ROUTING A
 # Prerequisites validation (complete power grid, no congestion)
 verifyConnectivity -type special
 
+# 1. Création de la règle
+add_ndr -name wide_enclosure_rule -width {MET1:MET4 0.9}
+
+# 2. Application aux signaux de données (on ignore l'horloge)
+set_interactive_constraint_modes [all_constraint_modes -active]
+set all_data_nets [get_nets * -filter "is_clock == false"]
+setAttribute -net $all_data_nets -non_default_rule wide_enclosure_rule
+set_interactive_constraint_modes {}
+
 # Main routing
 routeDesign
 
