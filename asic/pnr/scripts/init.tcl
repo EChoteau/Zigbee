17f111fdb5dfb586b05137a166addce846efe617
choteaue <choteau@grenoble-inp.fr>
2026-06-01 09:24:59 +0200

    add .lef en modify directly in it for pnr width of nets

--- asic/pnr/scripts/init.tcl
+++ asic/pnr/scripts/init.tcl
@@ -12,7 +12,7 @@ set init_mmmc_file "../scripts/view_definition.tcl"
 
 #set init_lef_file "../scripts/c35b4_A.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/CORELIB.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/IOLIB_4M.lef"
 #set init_lef_file "/softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/c35b4.lef_back /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/CORELIB.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/IOLIB_4M.lef"
-set init_lef_file "/softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/c35b4.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/CORELIB.lef"
+set init_lef_file "../input_data/c35b4.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/CORELIB.lef"
 if {$module_name eq "zigbee_top"} {
 	set init_lef_file "/softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/c35b4.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/CORELIB.lef /softslin/AMS_410_ISR15/cds/HK_C35/LEF/c35b4/IOLIB_4M.lef"
 }
