b2a30b6cab406f22528ef1fa48320318bf3a5b00
choteaue <choteau@grenoble-inp.fr>
2026-06-03 16:57:17 +0200

    Remove redundant ground net connection in design configuration and update ground net list in init script

--- asic/pnr/scripts/design_config.tcl
+++ asic/pnr/scripts/design_config.tcl
@@ -73,7 +73,6 @@ globalNetConnect vdd! -type pgpin -pin vdd3r1! -all
 globalNetConnect vdd! -type pgpin -pin vdd3r2! -all
 globalNetConnect vdd! -type pgpin -pin vdd3o!  -all
 globalNetConnect gnd! -type pgpin -pin gnd3r!  -all
-globalNetConnect gnd! -type pgpin -pin gnd3o!  -all
 globalNetConnect vdd! -type pgpin -pin A -inst PWR*
 globalNetConnect gnd! -type pgpin -pin A -inst GND*
 
