15b7e143c2c19079fa877b4016077ba1c6406816
choteaue <choteau@grenoble-inp.fr>
2026-06-09 16:17:07 +0200

    erf

--- asic/pnr/scripts/flow.tcl
+++ asic/pnr/scripts/flow.tcl
@@ -15,6 +15,7 @@ init_design
 # set init_ignore_pgpin_polarity_check "gnd!"
 read_power_intent -1801 ../scripts/zigbee_top_io.upf
 commit_power_intent
+saveDesign dbs/init_enc
 
 # ------------------------------------------------------------------------------
 # 2. FLOORPLAN & POWER GRID
