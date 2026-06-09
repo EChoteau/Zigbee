22f79c07c47f9563e9cd520c4f95560ad129e47d
choteaue <choteau@grenoble-inp.fr>
2026-06-09 10:49:00 +0200

    Update physical netlist generation options in final_steps.tcl for clarity and functionality

--- asic/pnr/scripts/final_steps.tcl
+++ asic/pnr/scripts/final_steps.tcl
@@ -36,15 +36,16 @@ saveNetlist "$output_root/${module_name}_postroute.v"
 
 # VIRTUOSO / LVS: Physical netlist with VDD/VSS connected throughout the design
 puts "=== Save Physical Netlist for Virtuoso/LVS ==="
-saveNetlist "zigbee_top_lvs.v" 
-#    -phys 
-# -includePowerGround \
-# -excludeCellInst {FILL25 FILL10 FILL5 FILL2 FILL1 \
-#                   FILLANT25 FILLANT10 FILLANT5 FILLANT2 FILLANT1 \
-#                   ENDCAPL ENDCAPR \
-#                   PERI_SPACER_100_P PERI_SPACER_50_P PERI_SPACER_20_P \
-#                   PERI_SPACER_10_P PERI_SPACER_5_P PERI_SPACER_2_P \
-#                   PERI_SPACER_1_P PERI_SPACER_01_P}
+saveNetlist "zigbee_top_lvs.v" \
+    -phys \
+    -includePowerGround \
+    -usePGPorts
+    # -excludeCellInst {FILL25 FILL10 FILL5 FILL2 FILL1 \
+    #                   FILLANT25 FILLANT10 FILLANT5 FILLANT2 FILLANT1 \
+    #                   ENDCAPL ENDCAPR \
+    #                   PERI_SPACER_100_P PERI_SPACER_50_P PERI_SPACER_20_P \
+    #                   PERI_SPACER_10_P PERI_SPACER_5_P PERI_SPACER_2_P \
+    #                   PERI_SPACER_1_P PERI_SPACER_01_P}
 
 write_sdf "$output_root/${module_name}_postroute.sdf"
 
