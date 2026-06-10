3e424ba4ad9d15b25dacb8610b0275c3e239fa32
choteaue <choteau@grenoble-inp.fr>
2026-06-10 09:36:09 +0200

    Update power and ground net names to include exclamation marks

--- asic/pnr/scripts/design_config.tcl
+++ asic/pnr/scripts/design_config.tcl
@@ -36,7 +36,7 @@ if {$module_name eq "zigbee_top"} {
 setAddRingMode -ring_target default -extend_over_row 0 -ignore_rows 0 -avoid_short 0 -skip_crossing_trunks none -stacked_via_top_layer MET4 -stacked_via_bottom_layer MET1 -via_using_exact_crossover_size true -orthogonal_only true -skip_via_on_pin {  standardcell } -skip_via_on_wire_shape {  noshape }
 
 #Ajoute la grille
-addRing -nets {gnd vdd} -type core_rings -follow core -layer {top MET3 bottom MET3 left MET4 right MET4} -width {top 20 bottom 20 left 20 right 20} -spacing {top 10 bottom 10 left 10 right 10} -offset {top 0.7 bottom 0.7 left 0.7 right 0.7} -center 1 -extend_corner {} -threshold 0 -jog_distance 0 -snap_wire_center_to_grid None
+addRing -nets {gnd! vdd!} -type core_rings -follow core -layer {top MET3 bottom MET3 left MET4 right MET4} -width {top 20 bottom 20 left 20 right 20} -spacing {top 10 bottom 10 left 10 right 10} -offset {top 0.7 bottom 0.7 left 0.7 right 0.7} -center 1 -extend_corner {} -threshold 0 -jog_distance 0 -snap_wire_center_to_grid None
 
 
 #Variables pour la création des stripes
@@ -60,29 +60,29 @@ set stripe_stop_offset 100
 setAddStripeMode -ignore_block_check true -break_at none -route_over_rows_only false -rows_without_stripes_only false -extend_to_closest_target none -stop_at_last_wire_for_area false -partial_set_thru_domain false -ignore_nondefault_domains false -trim_antenna_back_to_shape none -spacing_type edge_to_edge -spacing_from_block 5 -stripe_min_length 0 -stacked_via_top_layer MET4 -stacked_via_bottom_layer MET1 -via_using_exact_crossover_size false -split_vias false -orthogonal_only true -allow_jog { padcore_ring  block_ring }
 
 
-addStripe -nets {gnd vdd} -layer $stripe_layer -direction $stripe_direction -width $stripe_width -spacing $stripe_spacing -number_of_sets $nb_of_sets -start_from left -start_offset $stripe_start_offset -stop_offset $stripe_stop_offset -switch_layer_over_obs false -max_same_layer_jog_length 2 -padcore_ring_top_layer_limit MET4 -padcore_ring_bottom_layer_limit MET1 -block_ring_top_layer_limit MET4 -block_ring_bottom_layer_limit MET1 -use_wire_group 0 -snap_wire_center_to_grid None -skip_via_on_pin {  standardcell } -skip_via_on_wire_shape {  noshape }
+addStripe -nets {gnd! vdd!} -layer $stripe_layer -direction $stripe_direction -width $stripe_width -spacing $stripe_spacing -number_of_sets $nb_of_sets -start_from left -start_offset $stripe_start_offset -stop_offset $stripe_stop_offset -switch_layer_over_obs false -max_same_layer_jog_length 2 -padcore_ring_top_layer_limit MET4 -padcore_ring_bottom_layer_limit MET1 -block_ring_top_layer_limit MET4 -block_ring_bottom_layer_limit MET1 -use_wire_group 0 -snap_wire_center_to_grid None -skip_via_on_pin {  standardcell } -skip_via_on_wire_shape {  noshape }
 
 
 #////////////////////////////////////////////////////
 # ConnectGlobalNets
 #////////////////////////////////////////////////////
 
-globalNetConnect vdd -type pgpin -pin vdd! -all
-globalNetConnect gnd -type pgpin -pin gnd! -all
-globalNetConnect vdd -type pgpin -pin vdd3r1! -all
-globalNetConnect vdd -type pgpin -pin vdd3r2! -all
-globalNetConnect vdd -type pgpin -pin vdd3o!  -all
-globalNetConnect gnd -type pgpin -pin gnd3r!  -all
-globalNetConnect gnd -type pgpin -pin gnd3o!  -all
-globalNetConnect vdd -type pgpin -pin A -inst PWR*
-globalNetConnect gnd -type pgpin -pin A -inst GND*
+globalNetConnect vdd! -type pgpin -pin vdd! -all
+globalNetConnect gnd! -type pgpin -pin gnd! -all
+globalNetConnect vdd! -type pgpin -pin vdd3r1! -all
+globalNetConnect vdd! -type pgpin -pin vdd3r2! -all
+globalNetConnect vdd! -type pgpin -pin vdd3o!  -all
+globalNetConnect gnd! -type pgpin -pin gnd3r!  -all
+globalNetConnect gnd! -type pgpin -pin gnd3o!  -all
+globalNetConnect vdd! -type pgpin -pin A -inst PWR*
+globalNetConnect gnd! -type pgpin -pin A -inst GND*
 
 # applyGlobalNets
 
-add_text -layer MET4 -pt {50.0 885.0} -label vdd -drafting true
-add_text -layer MET4 -pt {890.0 1826.0} -label vdd -drafting true
-add_text -layer MET4 -pt {55.0 988.0} -label gnd -drafting true
-add_text -layer MET4 -pt {1823.0 983.0} -label gnd -drafting true
+add_text -layer MET4 -pt {50.0 885.0} -label vdd! -drafting true
+add_text -layer MET4 -pt {890.0 1826.0} -label vdd! -drafting true
+add_text -layer MET4 -pt {55.0 988.0} -label gnd! -drafting true
+add_text -layer MET4 -pt {1823.0 983.0} -label gnd! -drafting true
 
 #////////////////////////////////////////////////////
 # Special_route
@@ -91,7 +91,7 @@ add_text -layer MET4 -pt {1823.0 983.0} -label gnd -drafting true
 #Pour faire les stripe d'alimentation à l'horizontal
 setSrouteMode -viaConnectToShape { noshape }
 
-sroute -connect { blockPin padPin padRing corePin floatingStripe } -layerChangeRange { MET1 MET4 } -padPinPortConnect { allPort oneGeom } -padPinTarget { nearestTarget } -floatingStripeTarget { blockring padring ring stripe ringpin blockpin followpin } -allowJogging 1 -crossoverViaLayerRange { MET1 MET4 } -nets { gnd vdd } -allowLayerChange 1 -targetViaLayerRange { MET1 MET4 }
+sroute -connect { blockPin padPin padRing corePin floatingStripe } -layerChangeRange { MET1 MET4 } -padPinPortConnect { allPort oneGeom } -padPinTarget { nearestTarget } -floatingStripeTarget { blockring padring ring stripe ringpin blockpin followpin } -allowJogging 1 -crossoverViaLayerRange { MET1 MET4 } -nets { gnd! vdd! } -allowLayerChange 1 -targetViaLayerRange { MET1 MET4 }
 
 editPowerVia -add_vias 1
 
