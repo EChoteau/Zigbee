29d556eb2ba87d6b38ed45a38f285efa5dde00b0
choteaue <choteau@grenoble-inp.fr>
2026-06-11 10:19:55 +0200

    ddd

--- asic/pnr/scripts/final_steps.tcl
+++ asic/pnr/scripts/final_steps.tcl
@@ -36,7 +36,7 @@ saveNetlist "$output_root/${module_name}_postroute.v"
 
 # VIRTUOSO / LVS: Physical netlist with VDD/VSS connected throughout the design
 puts "=== Save Physical Netlist for Virtuoso/LVS ==="
-saveNetlist "zigbee_top_lvs.v" 
+saveNetlist "zigbee_top_lvs.v" -includePhysicalInst
  #   -phys \
 #    -includePowerGround \
     # -excludeCellInst {FILL25 FILL10 FILL5 FILL2 FILL1 \
